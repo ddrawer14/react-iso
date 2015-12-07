fa5b387f5f8c9ce0213bd086c0ee8f36c42ac249
ddrawer14 <ddrawer14@yahoo.com>
2015-12-06 23:38:21 -0500

    Rendering React Component on server. Univ JS! Working.

---
+++ client/components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {} from './style.less';
+
+export default React.createClass({
+
+  render: function() {
+    return <div>React Component defined on the client side, but also rendered on the server side! Universal Javascript</div>;
+  }
+});
---
+++ server/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import App from './generated/app';
+
+const app = express();
+
+app.get('/', (request, response) => {
+  //response.send('Hello world from Express');
+  response.send(ReactDOMServer.renderToString(<App />));
+});
+
+app.listen(3000, () => console.log('Server running'));
